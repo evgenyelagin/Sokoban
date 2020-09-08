5bd4f2d14dbbde7c648b4f4d0c0f70d1cf8358b0
evgenyelagin <anykeysky@gmail.com>
2020-09-08 14:59:38 +0300

    2. Сокобан. Тестовое задание.[75283#293324]

--- puzzle/src/main/java/ru/job4j/puzzle/Logic.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -66,19 +66,22 @@ public class Logic {
         for (int i = 0; i < table.length; i++) {
             boolean rowCheck = true;
             boolean columnCheck = true;
-                for (int j = 0; j < table.length; j++) {
-                    if ((columnCheck) && (1 != table[i][j])) {
-                        columnCheck = false;
-                    }
-                    if ((rowCheck) && (1 != table[j][i])) {
-                        rowCheck = false;
-                    }
+            for (int j = 0; j < table.length; j++) {
+                if ((columnCheck) && (1 != table[i][j])) {
+                    columnCheck = false;
                 }
-                if (rowCheck || columnCheck) {
-                    result = true;
+                if ((rowCheck) && (1 != table[j][i])) {
+                    rowCheck = false;
+                }
+                if (!rowCheck && !columnCheck) {
                     break;
                 }
             }
+            if (rowCheck || columnCheck) {
+                result = true;
+                break;
+            }
+        }
         return result;
     }
 
